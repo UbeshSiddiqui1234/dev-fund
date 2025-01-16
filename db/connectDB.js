b9bf29cf5853d37b4eee7797ed4d4c15999fd203
Ubesh siddiqui <ubeshsiddiqui402@gmail.com>
2025-01-17 00:42:17 +0530

    Update connectDB.js and .env.local for MongoDB connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        const conn = mongoose.connect("mongodb://localhost:27017/DevFund",{
+        const conn = mongoose.connect(process.env.MONGO_URL,{
             useNewUrlParser : true,
         })
     }
