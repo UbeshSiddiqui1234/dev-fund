b2dd31fd84a22672d98427131b7bb09f1600c052
Ubesh siddiqui <ubeshsiddiqui402@gmail.com>
2025-01-17 18:43:55 +0530

    updated connectDB file

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try{
         const conn = mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser : true,
-            useUnifiedTOpology: true,
             dbName: 'devfund'
         })
     }
